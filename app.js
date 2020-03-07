776206616fc2a17c523dc4efddb4c15545935ede
kujira <eco.server.record@gmail.com>
2020-03-08 00:55:53 +0900

    HTTPリクエストの試用

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+// 事前準備
+// yarnのインストール
+// curl -o- -L https://yarnpkg.com/install.sh | bash -s -- --version 1.13.0
+// PATHの適用
+// source ~/.bashrc
+// requestライブラリのインストール
+// yarn add request はエラーになった(VirtualにUbuntu環境)
+// yarn add request --no-bin-links で入った
+
 'use strict';
+const request = require('request'); //requestライブラリの取得
+request('https://www.google.com', (erroe, response, body) => {
+    console.log(body);
+})
